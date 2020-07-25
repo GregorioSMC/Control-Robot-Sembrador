59228804b9609f1a0eba0a44d7f13131f247aa59
GregorioSMC <48391784+GregorioSMC@users.noreply.github.com>
2020-07-24 19:30:52 -0500

    Robot Sembrador ITSSMT

---
+++ Mark_0_1/ControlPrincipal.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Mark_0_1
+{
+    partial class ControlPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlPrincipal));
+            this.btnAvanzar = new System.Windows.Forms.Button();
+            this.btnRetroceder = new System.Windows.Forms.Button();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.tkbDireccion = new System.Windows.Forms.TrackBar();
+            this.tkbVelocidad = new System.Windows.Forms.TrackBar();
+            this.lblDireccion = new System.Windows.Forms.Label();
+            this.lblVelocidad = new System.Windows.Forms.Label();
+            this.tmVoz = new System.Windows.Forms.Timer(this.components);
+            this.btnVozOn = new System.Windows.Forms.Button();
+            this.btnVozOff = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbDireccion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbVelocidad)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAvanzar
+            // 
+            this.btnAvanzar.Location = new System.Drawing.Point(28, 94);
+            this.btnAvanzar.Name = "btnAvanzar";
+            this.btnAvanzar.Size = new System.Drawing.Size(155, 62);
+            this.btnAvanzar.TabIndex = 0;
+            this.btnAvanzar.Text = "Avanzar";
+            this.btnAvanzar.UseVisualStyleBackColor = true;
+            this.btnAvanzar.Click += new System.EventHandler(this.btnAvanzar_Click);
+            // 
+            // btnRetroceder
+            // 
+            this.btnRetroceder.Location = new System.Drawing.Point(28, 261);
+            this.btnRetroceder.Name = "btnRetroceder";
+            this.btnRetroceder.Size = new System.Drawing.Size(155, 62);
+            this.btnRetroceder.TabIndex = 1;
+            this.btnRetroceder.Text = "Retroceder";
+            this.btnRetroceder.UseVisualStyleBackColor = true;
+            this.btnRetroceder.Click += new System.EventHandler(this.btnRetroceder_Click);
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(28, 180);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(155, 62);
+            this.btnDetener.TabIndex = 2;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.btnDetener_Click);
+            // 
+            // tkbDireccion
+            // 
+            this.tkbDireccion.Location = new System.Drawing.Point(201, 111);
+            this.tkbDireccion.Maximum = 2387;
+            this.tkbDireccion.Minimum = 1708;
+            this.tkbDireccion.Name = "tkbDireccion";
+            this.tkbDireccion.Size = new System.Drawing.Size(210, 45);
+            this.tkbDireccion.TabIndex = 3;
+            this.tkbDireccion.Value = 2048;
+            this.tkbDireccion.Scroll += new System.EventHandler(this.tkbDireccion_Scroll);
+            // 
+            // tkbVelocidad
+            // 
+            this.tkbVelocidad.Location = new System.Drawing.Point(201, 278);
+            this.tkbVelocidad.Maximum = 1023;
+            this.tkbVelocidad.Name = "tkbVelocidad";
+            this.tkbVelocidad.Size = new System.Drawing.Size(210, 45);
+            this.tkbVelocidad.TabIndex = 4;
+            this.tkbVelocidad.Scroll += new System.EventHandler(this.tkbVelocidad_Scroll);
+            // 
+            // lblDireccion
+            // 
+            this.lblDireccion.AutoSize = true;
+            this.lblDireccion.Location = new System.Drawing.Point(282, 91);
+            this.lblDireccion.Name = "lblDireccion";
+            this.lblDireccion.Size = new System.Drawing.Size(52, 13);
+            this.lblDireccion.TabIndex = 5;
+            this.lblDireccion.Text = "Direccion";
+            // 
+            // lblVelocidad
+            // 
+            this.lblVelocidad.AutoSize = true;
+            this.lblVelocidad.Location = new System.Drawing.Point(280, 265);
+            this.lblVelocidad.Name = "lblVelocidad";
+            this.lblVelocidad.Size = new System.Drawing.Size(54, 13);
+            this.lblVelocidad.TabIndex = 6;
+            this.lblVelocidad.Text = "Velocidad";
+            this.lblVelocidad.Click += new System.EventHandler(this.lblVelocidad_Click);
+            // 
+            // tmVoz
+            // 
+            this.tmVoz.Interval = 1000;
+            this.tmVoz.Tick += new System.EventHandler(this.tmVoz_Tick);
+            // 
+            // btnVozOn
+            // 
+            this.btnVozOn.Location = new System.Drawing.Point(530, 94);
+            this.btnVozOn.Name = "btnVozOn";
+            this.btnVozOn.Size = new System.Drawing.Size(155, 62);
+            this.btnVozOn.TabIndex = 7;
+            this.btnVozOn.Text = "Iniciar Voz";
+            this.btnVozOn.UseVisualStyleBackColor = true;
+            this.btnVozOn.Click += new System.EventHandler(this.btnVozOn_Click);
+            // 
+            // btnVozOff
+            // 
+            this.btnVozOff.Location = new System.Drawing.Point(530, 240);
+            this.btnVozOff.Name = "btnVozOff";
+            this.btnVozOff.Size = new System.Drawing.Size(155, 62);
+            this.btnVozOff.TabIndex = 8;
+            this.btnVozOff.Text = "Terminar Voz";
+            this.btnVozOff.UseVisualStyleBackColor = true;
+            this.btnVozOff.Click += new System.EventHandler(this.btnVozOff_Click);
+            // 
+            // ControlPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVozOff);
+            this.Controls.Add(this.btnVozOn);
+            this.Controls.Add(this.lblVelocidad);
+            this.Controls.Add(this.lblDireccion);
+            this.Controls.Add(this.tkbVelocidad);
+            this.Controls.Add(this.tkbDireccion);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.btnRetroceder);
+            this.Controls.Add(this.btnAvanzar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ControlPrincipal";
+            this.Text = "Control Principal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.tkbDireccion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbVelocidad)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAvanzar;
+        private System.Windows.Forms.Button btnRetroceder;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.TrackBar tkbDireccion;
+        private System.Windows.Forms.TrackBar tkbVelocidad;
+        private System.Windows.Forms.Label lblDireccion;
+        private System.Windows.Forms.Label lblVelocidad;
+        private System.Windows.Forms.Timer tmVoz;
+        private System.Windows.Forms.Button btnVozOn;
+        private System.Windows.Forms.Button btnVozOff;
+    }
+}
+
---
+++ Mark_0_1/ControlPrincipal.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using FireSharp;
+using ROBOTIS;
+
+namespace Mark_0_1
+{
+    public partial class ControlPrincipal : Form
+    {
+
+
+        IFirebaseConfig fireConfig = new FirebaseConfig
+        {
+            AuthSecret = "wgChIumVnCj8nHrEWhrHQ4PNMRreA4oy8lGuVuEu",
+            BasePath = "https://jarvis-c89ef.firebaseio.com/"
+        };
+
+        IFirebaseClient fireCliente;
+
+        public const int DEFAULT_PORTNUM = 20;
+        public const int DEFAULT_BAUDRATE = 1;
+        bool flagDireccion;
+        bool flagVelocidad;
+
+
+        public ControlPrincipal()
+        {
+            fireCliente = new FireSharp.FirebaseClient(fireConfig);
+
+            if (fireCliente != null)
+            {
+                MessageBox.Show("Existe Conexion con Firebase");
+            }
+            else
+            {
+                MessageBox.Show("Revisa la Conexion con Firebase");
+            }
+
+            InitializeComponent();
+            dynamixel.dxl_initialize(DEFAULT_PORTNUM, DEFAULT_BAUDRATE);
+        }
+
+        private void btnAvanzar_Click(object sender, EventArgs e)
+        {
+            Avanzar();
+        }
+
+
+        private void Avanzar()
+        {
+            flagDireccion = false;
+            flagVelocidad = false;
+            int dir = tkbDireccion.Value;
+            int velocidad = tkbVelocidad.Value;
+            dynamixel.dxl_write_word(1, 30, dir);
+            dynamixel.dxl_write_word(2, 30, dir);
+            dynamixel.dxl_write_word(3, 32, velocidad);
+            dynamixel.dxl_write_word(4, 32, velocidad | 0x400);
+            dynamixel.dxl_write_word(5, 32, velocidad);
+            dynamixel.dxl_write_word(6, 32, velocidad | 0x400);
+            dynamixel.dxl_write_word(10, 32, velocidad);
+        }
+
+        private void Detener()
+        {
+            tkbDireccion.Value = 2048;
+            dynamixel.dxl_write_word(1, 30, 2048);
+            dynamixel.dxl_write_word(2, 30, 2048);
+            dynamixel.dxl_write_word(3, 32, 0);
+            dynamixel.dxl_write_word(4, 32, 0);
+            dynamixel.dxl_write_word(5, 32, 0);
+            dynamixel.dxl_write_word(6, 32, 0);
+            dynamixel.dxl_write_word(10, 32, 0);
+        }
+
+        private void Retroceder()
+        {
+            flagDireccion = true;
+            flagVelocidad = true;
+            int dir = tkbDireccion.Value;
+            int velocidad = tkbVelocidad.Value;
+            dynamixel.dxl_write_word(1, 30, dir);
+            dynamixel.dxl_write_word(2, 30, dir);
+            dynamixel.dxl_write_word(3, 32, velocidad | 0x400);
+            dynamixel.dxl_write_word(4, 32, velocidad);
+            dynamixel.dxl_write_word(5, 32, velocidad | 0x400);
+            dynamixel.dxl_write_word(6, 32, velocidad);
+            dynamixel.dxl_write_word(10, 32, velocidad | 0x400);
+        }
+
+        private void btnDetener_Click(object sender, EventArgs e)
+        {
+            Detener();
+        }
+
+        private void tkbVelocidad_Scroll(object sender, EventArgs e)
+        {
+            int velocidad = tkbVelocidad.Value;
+            int dir = tkbDireccion.Value;
+
+            if (flagVelocidad)
+            {
+                dynamixel.dxl_write_word(1, 30, dir);
+                dynamixel.dxl_write_word(2, 30, dir);
+                dynamixel.dxl_write_word(3, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(4, 32, velocidad);
+                dynamixel.dxl_write_word(5, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(6, 32, velocidad);
+                dynamixel.dxl_write_word(10, 32, velocidad | 0x400);
+
+            }
+            else
+            {
+                dynamixel.dxl_write_word(1, 30, dir);
+                dynamixel.dxl_write_word(2, 30, dir);
+                dynamixel.dxl_write_word(3, 32, velocidad);
+                dynamixel.dxl_write_word(4, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(5, 32, velocidad);
+                dynamixel.dxl_write_word(6, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(10, 32, velocidad);
+            }
+
+        }
+
+        private void tmVoz_Tick(object sender, EventArgs e)
+        {
+            Voz();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            tmVoz.Enabled = false;
+        }
+
+        private async void Insertar()
+        {
+
+            var datex = new Datex
+            {
+                val = "null"
+            };
+
+            FirebaseResponse au = await fireCliente.UpdateAsync("Mark0/", datex);
+
+        }
+
+        internal class Datex
+        {
+            public string val { get; set; }
+        }
+
+
+        private async void Voz()
+        {
+            FirebaseResponse res = await fireCliente.GetAsync("Mark0/");
+            Datex std = res.ResultAs<Datex>();
+            string opc = std.val;
+
+            switch (opc)
+            {
+                case "0":                   
+                    Detener();
+                    break;
+
+                case "1":               
+                    Avanzar();                    
+                    break;
+
+                case "2":                    
+                    Retroceder();
+                    break;
+            }
+
+        }
+
+        private void btnVozOn_Click(object sender, EventArgs e)
+        {
+            tmVoz.Enabled = true;
+        }
+
+        private void btnVozOff_Click(object sender, EventArgs e)
+        {
+            tmVoz.Enabled = false;
+        }
+
+        private void btnRetroceder_Click(object sender, EventArgs e)
+        {
+            Retroceder();
+        }
+
+        private void lblVelocidad_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tkbDireccion_Scroll(object sender, EventArgs e)
+        {
+            int velocidad = tkbVelocidad.Value;
+            int dir = tkbDireccion.Value;
+
+            if (flagDireccion)
+            {
+                dynamixel.dxl_write_word(1, 30, dir);
+                dynamixel.dxl_write_word(2, 30, dir);
+                dynamixel.dxl_write_word(3, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(4, 32, velocidad);
+                dynamixel.dxl_write_word(5, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(6, 32, velocidad);
+                dynamixel.dxl_write_word(10, 32, velocidad | 0x400);
+
+            }
+            else
+            {
+                dynamixel.dxl_write_word(1, 30, dir);
+                dynamixel.dxl_write_word(2, 30, dir);
+                dynamixel.dxl_write_word(3, 32, velocidad);
+                dynamixel.dxl_write_word(4, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(5, 32, velocidad);
+                dynamixel.dxl_write_word(6, 32, velocidad | 0x400);
+                dynamixel.dxl_write_word(10, 32, velocidad);
+            }
+        }
+    }
+}
